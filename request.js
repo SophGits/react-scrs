f0dab6400375f8324d491930325e286a84dba0be
SophGits <sophie.jiffy@gmail.com>
2015-08-19 22:04:51 +0100

    Fail and AJAX and put failings on new branch.

--- request.js
+++ request.js
@@ -1,29 +1,53 @@
-(function(){
+// function getData( uri ){
 
-  var foo = 'https://builtvisible.com/messages-in-the-deep/';
-  var bar = {};
+//   var results = [];
 
-  $.ajax({
-    url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
-    data: {
-      'api_key': '65b608cf38b638ea71cf4c9baad182ae',
-      'url': foo
-    }
-  }).success( function( data ) {
-    console.log( "data: ", data);
-    bar = {
-      'fb-share' : data.Facebook.share_count ,
-      'twitter' : data.Twitter,
-      'linkedin' : data.LinkedIn
-    };
-    console.log( "results: ", bar );
-    return bar;
-  }).fail( function( error) {
-    console.log( "error: ", error );
-  })
-  .always( function() {
-    // alert( "complete" );
-  });
+//   $.ajax({
+//     url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
+//     data: {
+//       'api_key': '65b608cf38b638ea71cf4c9baad182ae',
+//       'url': uri
+//     }
+//   }).success( function( data ) {
+//     console.log( "data: ", data);
+//     info = {
+//       'fb-share' : data.Facebook.share_count,
+//       'twitter' : data.Twitter,
+//       'linkedin' : data.LinkedIn
+//     };
+//     console.log( "results: ", info );
+//     results.push(info);
+//   }).fail( function( error) {
+//     console.log( "error: ", error );
+//     return error;
+//   })
+//   .always( function( data ) {
+//     // alert( "complete" );
+//     console.log(data);
+//   });
+
+//   return results;
+// };
 
 
-})();
+
+ // $.ajax({
+ //      url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
+ //      data: {
+ //        'api_key': '65b608cf38b638ea71cf4c9baad182ae',
+ //        'url': this.state.uri
+ //      },
+ //      success: function( data ) {
+ //        var info = {
+ //          'fb-share' : data.Facebook.share_count,
+ //          'twitter' : data.Twitter,
+ //          'linkedin' : data.LinkedIn
+ //        };
+ //        console.log( "results: ", info );
+ //        this.setState({ "twitter": info.twitter, "fb-share": info["fb-share"] })
+ //      }.bind(this),
+ //      error: function( xhr, status, error) {
+ //        console.error(status, error.toString());
+ //        return error;
+ //      }.bind(this)
+ //    });
--- view.js
+++ view.js
@@ -2,40 +2,47 @@ var SocialCountResults = React.createClass({
   getInitialState: function() {
     return {
       test: "hello, world",
+      uri: "https://builtvisible.com/messages-in-the-deep/",
+      twitter: "",
       uriList: []
     };
   },
+  handleChange: function( event ) {
+    this.setState({ uri: event.target.value });
+  },
+  getData: function() {
+debugger
+
+    var apiKey = '65b608cf38b638ea71cf4c9baad182ae';
+    var url = 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json' + '&api_key=' + apiKey + '&url=' + this.state.uri
+    $.getJSON(url, function(result) {
+      alert("hi");
+      if(!result || !result.data || !result.data.length){
+        alert("error");
+          return;
+      }
+
+      var info = {
+        'fb-share' : result.Facebook.share_count,
+        'twitter' : result.Twitter,
+        'linkedin' : result.LinkedIn
+      };
+      this.setState({ "twitter": info.twitter, "fb-share": info["fb-share"] })
+    });
+
+  },
   render: function() {
     return (
-      <table>
-        <tr>
-          <th>URI</th>
-          <th>fb</th>
-          <th>twitter</th>
-          <th>linkedin</th>
-        </tr>
         <tr>
           <td className="uri">
-            <label htmlFor="unique">Looked-up URI</label>
-            <input type="text" name="" id="unique" value="Lookup URI" />
-            <button className="btn">Go</button>
+            <label htmlFor="unique2">{this.state.uri}</label>
+            <input type="text" id="unique2" value={this.state.uri} onChange={this.handleChange}  />
+            <button className="btn" onClick={this.getData}>Go</button>
           </td>
           <td className="fb">1</td>
-          <td className="twitter">2</td>
+          <td className="twitter">2 + {this.state.twitter}</td>
           <td className="linkedin">3</td>
         </tr>
-
-        <tr>
-          <td className="uri">
-            <label htmlFor="unique">{this.state.test}</label>
-            <input type="text" name="" id="unique" value={this.state.test} />
-            <button className="btn">Go</button>
-          </td>
-          <td className="fb">1</td>
-          <td className="twitter">2</td>
-          <td className="linkedin">3</td>
-        </tr>
-      </table>
     );
   }
 });

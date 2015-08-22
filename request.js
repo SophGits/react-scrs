a05b097339b2576e87d45a03fb72522a566103e5
Sophie <sophie@metabroadcast.com>
2015-08-22 11:54:01 +0100

    Put preventdefault on the bloody button forgoodnesssake.

--- request.js
+++ request.js
@@ -31,23 +31,4 @@
 
 
 
- // $.ajax({
- //      url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
- //      data: {
- //        'api_key': '65b608cf38b638ea71cf4c9baad182ae',
- //        'url': this.state.uri
- //      },
- //      success: function( data ) {
- //        var info = {
- //          'fb-share' : data.Facebook.share_count,
- //          'twitter' : data.Twitter,
- //          'linkedin' : data.LinkedIn
- //        };
- //        console.log( "results: ", info );
- //        this.setState({ "twitter": info.twitter, "fb-share": info["fb-share"] })
- //      }.bind(this),
- //      error: function( xhr, status, error) {
- //        console.error(status, error.toString());
- //        return error;
- //      }.bind(this)
- //    });
+ 
--- view.js
+++ view.js
@@ -10,25 +10,30 @@ var SocialCountResults = React.createClass({
   handleChange: function( event ) {
     this.setState({ uri: event.target.value });
   },
-  getData: function() {
-debugger
+  getData: function(e) {
+    e.preventDefault();
+    
+    $.ajax({
+       url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
+       data: {
+         'api_key': '65b608cf38b638ea71cf4c9baad182ae',
+         'url': this.state.uri
+       },
+       success: function( data ) {
 
-    var apiKey = '65b608cf38b638ea71cf4c9baad182ae';
-    var url = 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json' + '&api_key=' + apiKey + '&url=' + this.state.uri
-    $.getJSON(url, function(result) {
-      alert("hi");
-      if(!result || !result.data || !result.data.length){
-        alert("error");
-          return;
-      }
-
-      var info = {
-        'fb-share' : result.Facebook.share_count,
-        'twitter' : result.Twitter,
-        'linkedin' : result.LinkedIn
-      };
-      this.setState({ "twitter": info.twitter, "fb-share": info["fb-share"] })
-    });
+         var info = {
+           'fb-share' : data.Facebook.share_count,
+           'twitter' : data.Twitter,
+           'linkedin' : data.LinkedIn
+         };
+         console.log( "results: ", info );
+         this.setState({ "twitter": info.twitter, "fb-share": info["fb-share"] })
+       }.bind(this),
+       error: function( xhr, status, error) {
+         console.error(status, error.toString());
+         return error;
+       }.bind(this)
+     });
 
   },
   render: function() {

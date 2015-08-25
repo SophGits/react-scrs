54c6d4894a412ce0d565a8aa945ec734eca0398d
Sophie <sophie@metabroadcast.com>
2015-08-25 21:09:36 +0100

    Add ability to enter a uri, press the button and have it show up as a new entry in the view.

--- view.js
+++ view.js
@@ -1,20 +1,41 @@
 var SocialCountResults = React.createClass({
   getInitialState: function() {
     return {
-      uri: "https://builtvisible.com/messages-in-the-deep/",
-      twitter: "",
-      dataArray: [{Twitter: 56, url: 'www.hi.com', LinkedIn: 22, Facebook: { total_count: 12 } }, {Twitter: 5, url: 'www.hello.com', LinkedIn: 2, Facebook: { total_count: 3 } }]
+      blankData: { "Facebook": { "share_count": 0, "like_count": 0, "comment_count": 0, "total_count": 0}, "Twitter": 0, "LinkedIn": 0, "Pinterest": 0, "StumbleUpon": 0, "GooglePlusOne": 0, "BufferApp": 0, "url": ""}, 
+      newData: 
+        {
+          "Facebook": {
+            "share_count": 0,
+            "like_count": 0,
+            "comment_count": 0,
+            "total_count": 0
+          },
+          "Twitter": 0,
+          "LinkedIn": 0,
+          "Pinterest": 0,
+          "StumbleUpon": 0,
+          "GooglePlusOne": 0,
+          "BufferApp": 0,
+          "url": ""
+        }, 
+      dataArray: [
+        {Twitter: 56, url: 'www.hi.com', LinkedIn: 22, Facebook: { total_count: 12 } }, 
+        {Twitter: 5, url: 'www.hello.com', LinkedIn: 2, Facebook: { total_count: 3 } }
+      ]
     };
   },
   handleChange: function( event ) {
-    this.setState({ uri: event.target.value });
+    var wholeState = this.state;
+    wholeState.url = event.target.value;
+    // console.log(wholeState.url);
+    this.setState({ newData: wholeState });
   },
   foo: function (index, f) {
     console.log("something: ", index, f);
 
     var currentArray = this.state.dataArray;
     currentArray[index].url = f;
-// debugger
+
     this.setState({ dataArray: currentArray });
     this.getDataFromExistingRecord(index, f);
 
@@ -51,15 +72,16 @@ var SocialCountResults = React.createClass({
        url: 'https://cors-anywhere.herokuapp.com/zorg.builtvisible.com/tools/scrs/json',
        data: {
          'api_key': '65b608cf38b638ea71cf4c9baad182ae',
-         'url': this.state.uri
+         'url': this.state.newData.url
        },
        success: function( data ) {
 
          console.log( "results: ", data );
          // this.setState({ data: data })
 
-         var dataArray = this.state.dataArray.concat(data)
-
+        var dataArray = this.state.dataArray.concat(data);
+        this.setState({ dataArray: dataArray });
+        // this.render();
        }.bind(this),
        error: function( xhr, status, error) {
          console.error(status, error.toString());
@@ -74,13 +96,13 @@ var SocialCountResults = React.createClass({
         <ResultsList data={this.state.dataArray} onFoo={this.foo} />
         <tr>
           <td className="uri">
-            <label htmlFor="u2">{this.state.uri}</label>
-            <input type="text" id="u2" value={this.state.uri} onChange={this.handleChange}  />
+            <label htmlFor="u2">{this.state.newData.url}</label>
+            <input type="text" id="u2" value={this.state.newData.url} onChange={this.handleChange}  />
             <button className="btn" onClick={this.getData}>Go</button>
           </td>
-          <td className="fb">1</td>
-          <td className="twitter">{this.state.twitter}</td>
-          <td className="linkedin">3</td>
+          <td className="fb"></td>
+          <td className="twitter">{this.state.newData.Twitter}</td>
+          <td className="linkedin">{this.state.newData.LinkedIn}</td>
         </tr>
       </tbody>
     );

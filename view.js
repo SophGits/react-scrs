c2eaf945c6eb52aded877a6698037f4daaaec724
SophGits <sophie.jiffy@gmail.com>
2015-09-20 10:13:02 +0100

    Incorporate smallest component view into list view.

--- view.js
+++ view.js
@@ -1,8 +1,7 @@
 var SocialCountResults = React.createClass({
   getInitialState: function() {
     return {
-      blankData: { "Facebook": { "share_count": 0, "like_count": 0, "comment_count": 0, "total_count": 0}, "Twitter": 0, "LinkedIn": 0, "Pinterest": 0, "StumbleUpon": 0, "GooglePlusOne": 0, "BufferApp": 0, "url": ""}, 
-      newData: 
+      newData:
         {
           "Facebook": {
             "share_count": 0,
@@ -17,27 +16,21 @@ var SocialCountResults = React.createClass({
           "GooglePlusOne": 0,
           "BufferApp": 0,
           "url": ""
-        }, 
+        },
       dataArray: [
-        {Twitter: 56, url: 'www.hi.com', LinkedIn: 22, Facebook: { total_count: 12 } }, 
+        {Twitter: 56, url: 'www.hi.com', LinkedIn: 22, Facebook: { total_count: 12 } },
         {Twitter: 5, url: 'www.hello.com', LinkedIn: 2, Facebook: { total_count: 3 } }
-      ]
+      ] // this could be data from a db
     };
   },
-  handleChange: function( event ) {
-    var wholeState = this.state;
-    wholeState.url = event.target.value;
-    // console.log(wholeState.url);
-    this.setState({ newData: wholeState });
-  },
-  foo: function (index, f) {
-    console.log("something: ", index, f);
+  foo: function (index, uri) {
+    console.log("something: ", index, uri);
 
     var currentArray = this.state.dataArray;
-    currentArray[index].url = f;
+    currentArray[index].url = uri;
 
     this.setState({ dataArray: currentArray });
-    this.getDataFromExistingRecord(index, f);
+    this.getDataFromExistingRecord(index, uri);
 
     console.log(this.state.dataArray);
     // do setState instead
@@ -52,10 +45,9 @@ var SocialCountResults = React.createClass({
       },
       success: function( data ) {
         console.log( "results: ", data );
-        var orig = this.state.dataArray;
-        orig[index] = data;
-        this.setState({ dataArray: orig })
-        // debugger
+        var dataArray = this.state.dataArray;
+        dataArray[index] = data;
+        this.setState({ dataArray: dataArray })
         this.render();
       }.bind(this),
       error: function( xhr, status, error) {
@@ -63,7 +55,6 @@ var SocialCountResults = React.createClass({
         return error;
       }.bind(this)
     });
-
   },
   getData: function(e) {
     if (e) { e.preventDefault();}
@@ -79,8 +70,7 @@ var SocialCountResults = React.createClass({
          console.log( "results: ", data );
          // this.setState({ data: data })
 
-        var dataArray = this.state.dataArray;
-        dataArray.push(data);
+        var dataArray = this.state.dataArray.concat([data]);
         this.setState({ dataArray: dataArray });
         // this.render();
        }.bind(this),
@@ -91,26 +81,11 @@ var SocialCountResults = React.createClass({
      });
 
   },
-  yo: function(e) {
-    if (e.which === 13) {
-      e.preventDefault();
-      // this.foo(this.state.dataArray.index, e.target.value);
-    } 
-  },
   render: function() {
     return (
       <tbody>
-        <ResultsList data={this.state.dataArray} onFoo={this.foo} />
-        <tr>
-          <td className="uri">
-            <label htmlFor="u2">{this.state.newData.url}</label>
-            <input type="text" id="u2" value={this.state.newData.url} onChange={this.handleChange} onKeyPress={this.yo}  />
-            <button className="btn" onClick={this.getData}>Go</button>
-          </td>
-          <td className="fb"></td>
-          <td className="twitter">{this.state.newData.Twitter}</td>
-          <td className="linkedin">{this.state.newData.LinkedIn}</td>
-        </tr>
+        <ResultsList data={this.state.dataArray} onFoo={this.foo} storedData={this} getdata={this.getData} />
+
       </tbody>
     );
   }
@@ -120,48 +95,64 @@ var SocialCountResults = React.createClass({
 
 // render all results (in <tr></tr> nodes)
 var ResultsList = React.createClass({
+   hello: function(e) {
+    /*
+    e.preventDefault();
+    */
+    debugger
+    if (e.which === 13) {
+      e.preventDefault();
+      this.props.onFoo(this.props.index, e.target.value);
+    }
+  },
+  yo: function(e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      // this.foo(this.state.dataArray.index, e.target.value);
+    }
+  },
+  handleChange: function( event ) {
+    var wholeState = this.props.storedData.state;
+    wholeState.url = event.target.value;
+    // console.log(wholeState.url);
+    this.props.storedData.setState({ newData: wholeState });
+  },
   render: function() {
       var that = this;
+      {/* stored data from props */}
       var resultNodes = this.props.data.map(function(result, index) {
         return (
-          <Result obj={result} index={index} key={index} secondFoo={that.props.onFoo}></Result>
+          <tr className="Result">
+            <td className="uri">
+              <label htmlFor="unique2">{result.url}&nbsp;{index}&nbsp;</label>
+              <input type="text" id="unique2" defaultValue={result.url} onKeyPress={this.hello}  />
+              <button className="btn" onClick={this.hello}>Gogo</button>
+            </td>
+            <td className="fb">{result.Facebook.total_count}</td>
+            <td className="twitter">{result.Twitter}</td>
+            <td className="linkedin">{result.LinkedIn}</td>
+          </tr>
         );
       });
       return (
         <div className="resultList">
+          {/* using blank data to render empty input box */}
+          <tr>
+            <td className="uri">
+              <label htmlFor="u2">{this.props.storedData.state.newData.url}</label>
+              <input type="text" id="u2" value={this.props.storedData.state.newData.url} onChange={this.handleChange} onKeyPress={this.yo}  />
+              <button className="btn" onClick={this.props.getdata}>Go</button>
+            </td>
+            <td className="fb"></td>
+            <td className="twitter">{this.props.storedData.state.newData.Twitter}</td>
+            <td className="linkedin">{this.props.storedData.state.newData.LinkedIn}</td>
+          </tr>
           {resultNodes}
         </div>
       );
     }
 });
 
-// render the individual results in the store
-
-var Result = React.createClass({
-  hello: function(e) {
-    if (e.which === 13) {
-      e.preventDefault();
-      this.props.secondFoo(this.props.index, e.target.value);
-    } 
-  },
-  hi : function() {
-    console.log("because i have to provide change handler");
-    // this.props.obj.url = this.value;
-  },
-  render: function() {
-    return(
-    <tr className="Result">
-      <td className="uri">
-        <label htmlFor="unique2">{this.props.obj.url}&nbsp;{this.props.index}&nbsp;</label>
-        <input type="text" id="unique2" defaultValue={this.props.obj.url} onKeyPress={this.hello}  />
-        <button className="btn" onClick={this.getData}>Go</button>
-      </td>
-      <td className="fb">{this.props.obj.Facebook.total_count}</td>
-      <td className="twitter">{this.props.obj.Twitter}</td>
-      <td className="linkedin">{this.props.obj.LinkedIn}</td>
-    </tr>)
-  }
-});
 
 React.render(
   <SocialCountResults />,
